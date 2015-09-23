6c96d8d9f061f38b785d87efba9fe9bda2b226dd
Jonathan Channon <jonathan.channon@gmail.com>
2015-09-23 11:41:22 +0100

    initial

---
+++ openid/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace openid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var x = new Microsoft.IdentityModel.Tokens.Saml2SecurityTokenHandler();
+            Console.WriteLine("Eureka");
+            Console.ReadKey();
+        }
+    }
+}
